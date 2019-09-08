b27053424117e5b51c54502ca144933b76d840ec
Ramon Moraes <ramon.moraes94@gmail.com>
2019-09-07 22:07:42 -0300

    create example

--- neural/example.go
+++ neural/example.go
@@ -1,17 +1,42 @@
 package neural
 
+import "fmt"
+
 type perceptronTrainingData struct {
-	Input  []float32
-	Output bool
+	Input  []float64
+	Output float64
 }
 
 var biggerThenFiveDataset = []perceptronTrainingData{
-	perceptronTrainingData{[]float32{0, 0, 0}, false},
-	perceptronTrainingData{[]float32{0, 0, 1}, false},
-	perceptronTrainingData{[]float32{0, 1, 0}, false},
-	perceptronTrainingData{[]float32{0, 1, 1}, false},
-	perceptronTrainingData{[]float32{1, 0, 0}, false},
-	perceptronTrainingData{[]float32{1, 0, 1}, false},
-	perceptronTrainingData{[]float32{1, 1, 0}, true},
-	perceptronTrainingData{[]float32{1, 1, 1}, true},
+	perceptronTrainingData{[]float64{0, 0, 0}, 0},
+	perceptronTrainingData{[]float64{0, 0, 1}, 0},
+	perceptronTrainingData{[]float64{0, 1, 0}, 0},
+	perceptronTrainingData{[]float64{0, 1, 1}, 0},
+	perceptronTrainingData{[]float64{1, 0, 0}, 0},
+	perceptronTrainingData{[]float64{1, 0, 1}, 0},
+	perceptronTrainingData{[]float64{1, 1, 0}, 1},
+	perceptronTrainingData{[]float64{1, 1, 1}, 1},
+}
+
+func BiggerThenFiveExample() {
+	perceptron := NewPerceptron(3)
+	trainCount := 100
+
+	for trainCount > 0 {
+		trainCount--
+		for _, data := range biggerThenFiveDataset {
+			expected := data.Output
+			output := perceptron.Output(data.Input)
+			perceptron.Train(expected, output)
+		}
+	}
+
+	predict := perceptron.Output([]float64{1, 1, 1})
+	fmt.Println(predict)
+
+	predict = perceptron.Output([]float64{0, 0, 1})
+	fmt.Println(predict)
+
+	predict = perceptron.Output([]float64{0, 1, 1})
+	fmt.Println(predict)
 }
