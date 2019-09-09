b46b1445d2e60b4ec35f0f1f594ca0d3897fd1c4
Ramon Moraes <ramon.moraes94@gmail.com>
2019-09-09 09:54:11 -0300

    add bias and train method

--- neural/perceptron.go
+++ neural/perceptron.go
@@ -7,6 +7,7 @@ import (
 
 type Perceptron struct {
 	Weights []float64
+	Bias    float64
 }
 
 // NewPerceptron returns a Perceptron with initially randomized weights
@@ -17,8 +18,10 @@ func NewPerceptron(inputsAmount int8) Perceptron {
 		inputsAmount--
 		weights = append(weights, rand.Float64())
 	}
+
 	return Perceptron{
 		Weights: weights,
+		Bias:    1,
 	}
 }
 
@@ -28,13 +31,20 @@ func (p *Perceptron) activateFunction(sum float64) float64 {
 
 // Output is sum of the given inputs with it's weights
 func (p *Perceptron) Output(inputs []float64) float64 {
-	var output float64
+	var sum float64
 	for _, input := range inputs {
 		for _, weight := range p.Weights {
-			output += input * weight
+			sum += input * weight
 		}
 	}
-	return p.activateFunction(output)
+	return p.activateFunction(sum + p.Bias)
+}
+
+func stepFunction(sum float64, treshold float64) float64 {
+	if sum > treshold {
+		return 1
+	}
+	return 0
 }
 
 func sigmoid(sum float64) float64 {
@@ -43,4 +53,13 @@ func sigmoid(sum float64) float64 {
 
 func sigmoidDerivated(number float64) float64 {
 	return number * (number - 1)
+}
+
+func (p *Perceptron) Train(inputs []float64, expectedOutput float64) {
+	const learningRate = 0.1
+	outputErr := expectedOutput - p.Output(inputs)
+	p.Bias += outputErr
+	for i, _ := range p.Weights {
+		p.Weights[i] -= outputErr
+	}
 }
